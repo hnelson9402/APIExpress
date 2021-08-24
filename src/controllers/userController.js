7445c1c5d6860347ee530635919a9adfb23f1d91
hugo <hnelsontorres1994@gmail.com>
2021-08-23 19:42:28 -0500

    se termina proceso para registrar un usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import connection from '../db/connectionBD.js'
 import error from '../config/error.js';
 import { number , generateToken , generatePassword , comparePassword } from '../config/validatorHNPT.js';
@@ -38,11 +39,23 @@ controller.user = async (req, res) => {
 
 //save new user
 controller.save = async (req , res) =>{
-     const { nombre,apellido,cedula,correo,rol,estado,password } = req.body; 
-     let IDToken = generateToken(cedula,correo);
-     let passEnc = await generatePassword(password);     
-
-     //res.json(passEnc);
+    try {        
+        const { nombre,apellido,cedula,correo,rol,estado,password } = req.body; 
+        let IDToken = generateToken(cedula,correo);
+        let passEnc = await generatePassword(password);     
+        moment.locale('col');
+        let fecha = moment().format('YYYY-MM-DD HH:mm:ss');
+        let data = [nombre,apellido,cedula,correo,rol,estado,passEnc,IDToken,fecha];
+        
+        let results = await connection.awaitQuery('INSERT INTO usuario (nombre,apellido,cedula,correo,rol,estado,password,IDToken,fecha) VALUES (?,?,?,?,?,?,?,?,?)',data);
+        if (results.affectedRows > 0) {            
+            res.status(200).json(error('ok','Usuario registrado'));
+        } else {
+            res.status(400).json(error('error','No se puede registrar el usuario')); 
+        }
+    } catch (err) {
+        res.status(500).json(error('error', err));   
+    }      
 };
 
 
